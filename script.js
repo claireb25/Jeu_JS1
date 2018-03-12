a6ac237d1e1db837966c8e62dd9b600c1a69402b
claireb25 <claire.bourgeois.cb@gmail.com>
2018-03-12 17:18:51 +0100

    modifications de l'affichage input

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ var nb_random = Math.floor(Math.random() * (max - min) + min);
 console.log(nb_random);
 var coups = 0;
 
-
 var bouton = document.getElementById('clickme');
 
 bouton.addEventListener('click', function(){
-    
+    document.getElementById("clickme").value = "Valider";
+
     var saisie = document.getElementById('saisie').value ;  /*valeur saisie */
         coups++
     
@@ -21,15 +21,22 @@ bouton.addEventListener('click', function(){
             if (saisie<nb_random){
                 document.getElementById('result').innerHTML = "";
                 document.getElementById('result').innerHTML = "Cherhe plus grand !";
+                document.getElementById('saisie').value ="";   
             }
 
             else if(saisie>nb_random){
                 document.getElementById('result').innerHTML = "";
                 document.getElementById('result').innerHTML ="Cherche plus petit";
+                document.getElementById('saisie').value ="";   
             }
             else if(saisie==nb_random){
                 document.getElementById('result').innerHTML = "";
                 document.getElementById('result').innerHTML ="Bravo tu as réussi en "+coups+" essais !";
+                document.getElementById("clickme").value = "Recommencer";
+                nb_random = Math.floor(Math.random() * (max - min) + min); 
+                document.getElementById('saisie').value ="";   
+                document.getElementById('saisie').placeholder="Entre une nouvelle valeur";     
+                coups=0;       
             }  
         }    
 });
