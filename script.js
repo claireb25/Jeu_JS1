f56a35bdb22e91c6dfb9b9250894db1637d33be6
claireb25 <claire.bourgeois.cb@gmail.com>
2018-03-12 17:43:39 +0100

    erreurs js

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
-/*var nb_random = Math.floor(Math.random()*100) + 1;*/
-var max = 100 ;
-var min = 1
-var nb_random = Math.floor(Math.random() * (max - min) + min);
-console.log(nb_random);
-var coups = 0;
+document.addEventListener("DOMContentLoaded", function() {
+    /*var nb_random = Math.floor(Math.random()*100) + 1;*/
+    var max = 100;
+    var min = 1;
+    var nb_random = Math.floor(Math.random() * (max - min) + min);
+    var coups = 0;
 
-var bouton = document.getElementById('clickme');
+    var bouton = document.getElementById('clickme');
 
-bouton.addEventListener('click', function(){
-    document.getElementById("clickme").value = "Valider";
+    bouton.addEventListener('click', function(){
+        document.getElementById("clickme").value = "Valider";
 
-    var saisie = document.getElementById('saisie').value ;  /*valeur saisie */
-        coups++
-    
-        console.log(coups)
-        if (saisie>max){
-            document.getElementById('result').innerHTML ="Attention ce nombre n'est pas compris entre 1 et 100";
-        }
-        else {
-            if (saisie<nb_random){
-                document.getElementById('result').innerHTML = "";
-                document.getElementById('result').innerHTML = "Cherhe plus grand !";
-                document.getElementById('saisie').value ="";   
+        var saisie = document.getElementById('saisie').value;  /*valeur saisie */
+            coups++
+            if (saisie>max){
+                document.getElementById('result').innerHTML ="Attention ce nombre n'est pas compris entre 1 et 100";
             }
+            else {
+                if (saisie<nb_random){
+                    document.getElementById('result').innerHTML = "";
+                    document.getElementById('result').innerHTML = "Cherhe plus grand !";
+                    document.getElementById('saisie').value ="";   
+                }
 
-            else if(saisie>nb_random){
-                document.getElementById('result').innerHTML = "";
-                document.getElementById('result').innerHTML ="Cherche plus petit";
-                document.getElementById('saisie').value ="";   
-            }
-            else if(saisie==nb_random){
-                document.getElementById('result').innerHTML = "";
-                document.getElementById('result').innerHTML ="Bravo tu as réussi en "+coups+" essais !";
-                document.getElementById("clickme").value = "Recommencer";
-                nb_random = Math.floor(Math.random() * (max - min) + min); 
-                document.getElementById('saisie').value ="";   
-                document.getElementById('saisie').placeholder="Entre une nouvelle valeur";     
-                coups=0;       
-            }  
-        }    
+                else if(saisie>nb_random){
+                    document.getElementById('result').innerHTML = "";
+                    document.getElementById('result').innerHTML ="Cherche plus petit";
+                    document.getElementById('saisie').value ="";   
+                }
+                else if(saisie==nb_random){
+                    document.getElementById('result').innerHTML = "";
+                    document.getElementById('result').innerHTML ="Bravo tu as réussi en "+coups+" essais !";
+                    document.getElementById("clickme").value = "Recommencer";
+                    nb_random = Math.floor(Math.random() * (max - min) + min); 
+                    document.getElementById('saisie').value ="";   
+                    document.getElementById('saisie').placeholder="Entre une nouvelle valeur";     
+                    coups=0;       
+                }  
+            }    
+    });
 });
-
