eca829ccaebf2f6a5bbf70b7d47edd26834422d6
claireb25 <claire.bourgeois.cb@gmail.com>
2018-03-12 16:46:05 +0100

    ajout compteur et max 100

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-
-var nb_random = Math.floor(Math.random()*100);
+/*var nb_random = Math.floor(Math.random()*100) + 1;*/
+var max = 100 ;
+var min = 1
+var nb_random = Math.floor(Math.random() * (max - min) + min);
 console.log(nb_random);
+var coups = 0;
+
 
 var bouton = document.getElementById('clickme');
 
-bouton.addEventListener('click', function()
-{
+bouton.addEventListener('click', function(){
+    
     var saisie = document.getElementById('saisie').value ;  /*valeur saisie */
-
-        if (saisie<nb_random){
-            document.getElementById('result').innerHTML = "";
-            document.getElementById('result').innerHTML = "C'est plus :-)";
+        coups++
+    
+        console.log(coups)
+        if (saisie>max){
+            document.getElementById('result').innerHTML ="Attention ce nombre n'est pas compris entre 1 et 100";
         }
+        else {
+            if (saisie<nb_random){
+                document.getElementById('result').innerHTML = "";
+                document.getElementById('result').innerHTML = "Cherhe plus grand !";
+            }
 
-        else if(saisie>nb_random){
-            document.getElementById('result').innerHTML = "";
-            document.getElementById('result').innerHTML ="C'est moins :-(";
-        }
-        else if(saisie==nb_random){
-            document.getElementById('result').innerHTML = "";
-            document.getElementById('result').innerHTML ="Bravo tu as réussi en + nb_essais + essais !";
-        }      
-}
-);
+            else if(saisie>nb_random){
+                document.getElementById('result').innerHTML = "";
+                document.getElementById('result').innerHTML ="Cherche plus petit";
+            }
+            else if(saisie==nb_random){
+                document.getElementById('result').innerHTML = "";
+                document.getElementById('result').innerHTML ="Bravo tu as réussi en "+coups+" essais !";
+            }  
+        }    
+});
 
